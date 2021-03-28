e0757910297e308e96c854de7e15584826fbf6ee
Megs <Megs.close@gmail.com>
2021-03-28 16:52:08 -0700

    questions added

--- index.js
+++ index.js
@@ -1,7 +1,131 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub user name. (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("You must enter your project's name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("You must enter your project's name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("You must enter your project's name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project. (Required)', 
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("You must enter your project's name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInstallInfo',
+        message: 'Would you like to share installation instructions or information?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please share installation instructions or information.',
+        when: ({ confirmInstallInfo }) => confirmInstallInfo
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to share how your project is used?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please share usage information for your program.',
+        when: ({ confirmUsage }) => confirmUsage
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContribution',
+        message: 'Are you open to contributions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please share what your requirements are for excepting contributions.',
+        when: ({ confirmContribution }) => confirmContribution
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAcknowledgment',
+        message: 'Do you have people or other applications that you would like to acknowledge for their contributions to your project?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'acknowledgment',
+        message: 'Please share any additional authors, contributors, or applications that helped you build your project.',
+        when: ({ confirmAcknowledgment }) => confirmAcknowledgment
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTesting',
+        message: 'Would you like to give instructions on how to run tests for your project/application?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please share testing instructions',
+        when: ({ confirmTesting }) => confirmTesting
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please choose a license that your application is covered under.',
+        choices: ['APM', 'AUR license', 'Bower', 'Cocoapods', 'Conda - License', 'CPAN', 'CRAN/METACRAN', 'Crates.io', 'CTAN', 'DUB', 'Eclipse Marketplace', 'GitHub', 'Hex.pm', 'NPM', 'Ore License', 'Packagist License', 'PyPI', 'MIT']
+    },
+];
+
+const promptUser = () => {
+    return inquirer.prompt(questions);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +135,7 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+console.log("node working!");
+
+promptUser();
